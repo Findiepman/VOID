82e3f6930fc3a05b5e0c629b27422fad2e2edba6
Diepman Fin <18620@waerdenborch.nl>
2026-03-25 09:14:26 +0100

    Added desktop background

--- src/components/desktop.tsx
+++ src/components/desktop.tsx
@@ -5,10 +5,15 @@ import { Monitor, Folder, Settings, X } from 'lucide-react';
 export function Desktop() {
     return (
         <main>
-            <Monitor size={64} className="text-blue-400 transition-colors cursor-pointer" />
-            <Folder size={64} className="text-yellow-500 transition-colors cursor-pointer"/>
-            <Settings size={64} className="text-gray-500 transition-colors cursor-pointer"/>
-            <X size={64} className="text-black-500 transition-colors cursor-pointer"/>
+            <div className="h-screen w-screen bg-[url('public/tuf-sigma.jpg')] bg-cover bg-center overflow-hidden">
+                <Monitor size={64} className="text-blue-400 transition-colors cursor-pointer" />
+                <Folder size={64} className="text-yellow-500 transition-colors cursor-pointer" />
+                <Settings size={64} className="text-gray-500 transition-colors cursor-pointer" />
+                <X size={64} className="text-black-500 transition-colors cursor-pointer" />
+            </div>
+
+
+
         </main>
     )
 }
