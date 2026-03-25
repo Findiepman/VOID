0a6adb5e2ae67de2cdc895ca09aa062331caf97b
Slot Thomas <18606@waerdenborch.nl>
2026-03-25 09:40:01 +0100

    Taskbar 2

--- src/components/desktop.tsx
+++ src/components/desktop.tsx
@@ -7,12 +7,11 @@ export function Desktop() {
     return (
         <main>
             <div className="h-screen w-screen bg-[url('public/nature.jpg')] bg-cover bg-center overflow-hidden">
-                <Monitor size={64} className="text-blue-400 transition-colors cursor-pointer" />
-                <Folder size={64} className="text-yellow-500 transition-colors cursor-pointer" />
-                <Settings size={64} className="text-gray-500 transition-colors cursor-pointer" />
-                <X size={64} className="text-black-500 transition-colors cursor-pointer" />
+                <Monitor size={64} className="text-blue-400 cursor-pointer hover:text-blue-300 mx-1" />
+                <Folder size={64} className="text-yellow-500 cursor-pointer hover:text-yellow-400 mx-1" />
+                <Settings size={64} className="text-gray-300 cursor-pointer hover:text-gray-100 mx-1" />
                 <Taskbar />
-            </div>        
+            </div>
         </main>
     )
 }
