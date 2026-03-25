c43616b0b40d66048922dc4a9e5785633a8ba9e3
Slot Thomas <18606@waerdenborch.nl>
2026-03-25 09:58:55 +0100

    heb taskbar gemaakt met icoontjes

--- src/components/taskbar.tsx
+++ src/components/taskbar.tsx
@@ -1,22 +1,37 @@
 interface TaskbarProps {
     onStartClick: () => void
 }
-import { Monitor, Folder, Settings, X } from 'lucide-react';
+import { Monitor, Folder, Settings, X} from 'lucide-react';
 
 const iconen = [Monitor, Folder, Settings];
 
 export function Taskbar({ onStartClick }: TaskbarProps) {
     return (
-        <div className="bg-gray-800/90 backdrop-blur-md w-full absolute bottom-0 flex items-center px-4 h-12 border-t border-white/10 z-50">
-            {iconen.map((Icoon, index) => (
+        <div className="bg-gray-800/90 backdrop-blur-md w-full absolute bottom-0 flex items-center px-4 h-12 justify-center border-t border-white/10 z-50">
                 <button
-                    key={index}
                     onClick={onStartClick}
                     className="hover:bg-white/10 p-1 rounded transition-colors mx-1"
                 >
-                    <Icoon size={32} className="text-slate-200" />
+                    <Monitor size={32} className="text-blue-400 transition-colors cursor-pointer" />                
                 </button>
-            ))}
+                <button
+                    onClick={onStartClick}
+                    className="hover:bg-white/10 p-1 rounded transition-colors mx-1"
+                >
+                    <Folder size={32} className="text-yellow-500 transition-colors cursor-pointer" />                
+                </button>
+                <button
+                    onClick={onStartClick}
+                    className="hover:bg-white/10 p-1 rounded transition-colors mx-1"
+                >
+                    <Settings size={32} className="text-gray-500 transition-colors cursor-pointer" />                
+                </button>
+                <button
+                    onClick={onStartClick}
+                    className="hover:bg-white/10 p-1 rounded transition-colors mx-1"
+                >
+                    <X size={32} className="text-black-500 transition-colors cursor-pointer" />                
+                </button>
         </div>
     );
 }
